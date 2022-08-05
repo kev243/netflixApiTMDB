72216a0680cc7bbbfd8c39ee49e2babf55cd893a
nimikevin <97411659+nimikevin@users.noreply.github.com>
2022-08-04 22:37:37 -0400

    first commit

---
+++ lib/ui/screens/home_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:notnetflix/utils/constant.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        leading: Image.asset(
+          'assets/images/Netflix.png',
+        ),
+       
+      ),
+      body: ListView(
+        children: [
+          SizedBox(height: 15),
+          Container(
+            height: 500,
+            color: Colors.red,
+          ),
+          const SizedBox(height: 15),
+          Text('Tendancs actuelles',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              )),
+          const SizedBox(height: 15),
+          SizedBox(
+            height: 160,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: EdgeInsets.only(right: 8),
+                    width: 110,
+                    color: Colors.yellow,
+                    child: Center(
+                      child: Text(index.toString()),
+                    ),
+                  );
+                }),
+          ),
+          SizedBox(height: 15),
+          const SizedBox(height: 15),
+          Text('Actuellement en cinéma',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              )),
+          const SizedBox(height: 15),
+          SizedBox(
+            height: 320,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: EdgeInsets.only(right: 8),
+                    width: 220,
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(index.toString()),
+                    ),
+                  );
+                }),
+          ),
+          SizedBox(height: 15),
+          const SizedBox(height: 15),
+          Text('Ils arrivent bientôt',
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              )),
+          const SizedBox(height: 15),
+          SizedBox(
+            height: 160,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: EdgeInsets.only(right: 8),
+                    width: 110,
+                    color: Colors.green,
+                    child: Center(
+                      child: Text(index.toString()),
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+//Les couleurs 
+Color kBackgroundColor= const Color(0xff020202);
+Color kPrimaryColor= const Color(0xffe50914);
